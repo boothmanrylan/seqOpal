76766ae82b765abb082106e54746bf426ecc014b
Rylan Boothman <boothmanrylan@gmail.com>
2020-04-20 21:30:49 +0000

    bug fixes

--- run_many.sh
+++ run_many.sh
@@ -1,23 +1,25 @@
-modelfile=./model
-traindata=../data/A1/train/A1.train.fasta
-trainlabels=../data/A1/train/A1.train.taxid
-rm $modelfile
+disk=/mnt/disks/disk-1
+traindata=$disk/fasta/example.train.fasta
+trainlabels=$disk/fasta/example.train.taxid
+modelfile=$disk/model
+ldpcfile=$disk/ldpc.matrix
+rm $modelfile $ldpcfile
 echo start batch 1
-/usr/bin/time -v \
-    seqc main.seq $traindata -l $trainlabels -c 1 -S \
-        | vw -f $modelfile --save_resume --oaa 10 --bit_precision 30
+time \
+    ./seqc main.seq $traindata -l $trainlabels -c 1 -f 200 -s $ldpcfile \
+        | vw -f $modelfile --save_resume --oaa 193 --bit_precision 31 --loss_function=logistic
 echo finished batch 1
 echo
 echo
-counter=2
-while [ $counter -le 10 ]
-do
-    echo start batch $counter
-    /usr/bin/time -v \
-        seqc main.seq $traindata -l $trainlabels -c 1 -S \
-            | vw -i $modelfile -f $modelfile --save_resume --oaa 10 --bit_precision 30
-    echo finished batch $counter
-    ((counter++))
-    echo
-    echo
-done
+#counter=15
+#while [ $counter -le 15 ]
+#do
+#    echo start batch $counter
+#    time \
+#        ./seqc main.seq $traindata -l $trainlabels -c 1 -f 200 -r $ldpcfile \
+#            | vw -i $modelfile -f $modelfile --save_resume --oaa 193 --bit_precision 31 --loss_function=logistic
+#    echo finished batch $counter
+#    ((counter++))
+#    echo
+#    echo
+#done
