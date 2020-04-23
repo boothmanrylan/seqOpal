69f65b63f50d01be00e62e2896a8713980c6828a
Rylan Boothman <boothmanrylan@gmail.com>
2020-04-23 19:19:16 +0000

    switch from coverage 1 epochs 15 to coverage 15 epochs 1

--- run_experiment.sh
+++ run_experiment.sh
@@ -6,8 +6,8 @@ testdata=/mnt/disks/disk-1/data/A1/test/A1.test.fasta
 testlabels=/mnt/disks/disk-1/data/A1/test/A1.test.taxid
 predictionfile=/mnt/disks/disk-1/predictions
 
-time ./seqc train.seq $traindata $trainlabels -c 1 -e 15 -s -r -f 100 -S $ldpcfile \
+time seqc train.seq $traindata $trainlabels -k 64 -c 15 -e 1 -s -r -f 64 -S $ldpcfile \
 	| vw -f $modelfile --oaa 10 --bit_precision 31
-time ./seqc predict.seq $testdata -L $ldpcfile -r \
+time seqc predict.seq $testdata -L $ldpcfile -r -k 64 \
 	| vw -i $modelfile -p $predictionfile
-time ./seqc accuracy.seq $testlabels $predictionfile
+time seqc accuracy.seq $testlabels $predictionfile
