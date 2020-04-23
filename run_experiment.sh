64c8ced11758fea512bc30d6c7f81d1b1130037d
Rylan Boothman <boothmanrylan@gmail.com>
2020-04-23 00:59:41 +0000

    change files to point to disk-1

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,11 +1,13 @@
-modelfile=./model
-ldpcfile=./ldpc
-traindata=../data/A1/train/A1.train.fasta
-trainlabels=../data/A1/train/A1.train.taxid
-testdata=../data/A1/test/A1.test.fasta
-testlabels=../data/A1/test/A1.test.taxid
-predictionfile=./predictions
+modelfile=/mnt/disks/disk-1/model
+ldpcfile=/mnt/disks/disk-1/ldpc.matrix
+traindata=/mnt/disks/disk-1/data/A1/train/A1.train.fasta
+trainlabels=/mnt/disks/disk-1/data/A1/train/A1.train.taxid
+testdata=/mnt/disks/disk-1/data/A1/test/A1.test.fasta
+testlabels=/mnt/disks/disk-1/data/A1/test/A1.test.taxid
+predictionfile=/mnt/disks/disk-1/predictions
 
-seqc train.seq $traindata $trainlabels -c 1 -e 15 -s -S $ldpcfile | vw -f $modelfile -oaa 10 --bit_precision 31
-seqc predict.seq $testdata -L $ldpcfile | vw -i $modelfile -p $predictionfile
-seqc accuracy.seq $testlabels $predictions
+time ./seqc train.seq $traindata $trainlabels -c 1 -e 15 -s -r -f 100 -S $ldpcfile \
+	| vw -f $modelfile --oaa 10 --bit_precision 31
+time ./seqc predict.seq $testdata -L $ldpcfile -r \
+	| vw -i $modelfile -p $predictionfile
+time ./seqc accuracy.seq $testlabels $predictionfile
